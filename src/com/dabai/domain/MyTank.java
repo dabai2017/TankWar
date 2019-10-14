c2a49e4b03dcceb061992b50c42c988117fe8d09
dabai2017 <1904812793@qq.com>
2019-10-14 20:20:37 +0800

    刚刚显示出基础小坦克

--- src/com/dabai/domain/MyTank.java
+++ src/com/dabai/domain/MyTank.java
@@ -1,11 +1,14 @@
 package com.dabai.domain;
 
+import com.dabai.utils.Direction;
+
 public class MyTank extends Element{
 
 	private int blood;
 	private int speed;
 	private int power;
 	
+	private Direction direction;
 	
 	public MyTank(String imgPath, int x, int y) {
 		super(imgPath, x, y);
--- src/com/dabai/utils/GameWindow.java
+++ src/com/dabai/utils/GameWindow.java
@@ -10,116 +10,110 @@ import org.lwjgl.input.Keyboard;
 import com.dabai.domain.*;
 import com.dabai.game.Config;
 
-public class GameWindow extends Window{
+public class GameWindow extends Window {
 
-	
-		//新建一组砖墙对象
-		private ArrayList<Element> mElementList = new ArrayList<>();
-		
-	
+	// 新建一组砖墙对象
+	private ArrayList<Element> mElementList = new ArrayList<>();
+
+	private MyTank myTank;
+
 	public GameWindow(String title, int width, int height, int fps) {
 		super(title, width, height, fps);
-		
+
 	}
 
 	@Override
 	protected void onCreate() {
-		
-
-		
 
 		try {
-			//SoundUtils.play("res\\snd\\start.wav");
+			// SoundUtils.play("res\\snd\\start.wav");
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
-		//窗体宽度/px = 一个窗体可以摆放几块
-		//土墙
-		for (int i = 0; i <Config.WIDTH/Config.PX-1; i++) {
-				                        //  图片位子                           x轴               y轴
-			Wall wall = new Wall("res\\img\\wall.gif", i*Config.PX, Config.PX);
+		// 窗体宽度/px = 一个窗体可以摆放几块
+		// 土墙
+		for (int i = 0; i < Config.WIDTH / Config.PX - 1; i++) {
+			// 图片位子 x轴 y轴
+			Wall wall = new Wall("res\\img\\wall.gif", i * Config.PX, Config.PX);
 			mElementList.add(wall);
-			}
-		//水墙
-		for (int i = 1; i < Config.WIDTH/Config.PX; i++) {
-			Water water = new Water("res\\img\\water.gif", i*Config.PX, Config.PX*3);
+		}
+		// 水墙
+		for (int i = 1; i < Config.WIDTH / Config.PX; i++) {
+			Water water = new Water("res\\img\\water.gif", i * Config.PX,
+					Config.PX * 3);
 			mElementList.add(water);
 		}
-		//铁墙
-		for (int i = 0; i < Config.WIDTH/Config.PX-1; i++) {
-			Steel steel = new Steel("res\\img\\steel.gif", i*Config.PX, Config.PX*5);
+		// 铁墙
+		for (int i = 0; i < Config.WIDTH / Config.PX - 1; i++) {
+			Steel steel = new Steel("res\\img\\steel.gif", i * Config.PX,
+					Config.PX * 5);
 			mElementList.add(steel);
 		}
-		//草墙
-		for (int i = 1; i < Config.WIDTH/Config.PX; i++) {
-			Grass grass = new Grass("res\\img\\grass.gif", i*Config.PX, Config.PX*7);
+		// 草墙
+		for (int i = 1; i < Config.WIDTH / Config.PX; i++) {
+			Grass grass = new Grass("res\\img\\grass.gif", i * Config.PX,
+					Config.PX * 7);
 			mElementList.add(grass);
 		}
-		
+
+		myTank = new MyTank("res\\img\\tank_u.gif", Config.WIDTH / 2
+				- Config.PX / 2, Config.HEIGHT - Config.PX);
+
 	}
 
 	@Override
 	protected void onMouseEvent(int key, int x, int y) {
-		
-		
+
 	}
 
 	@Override
 	protected void onKeyEvent(int key) {
-		
-		
-		
-		
-		
-		//上下左右事件
+
+		// 上下左右事件
 		if (key == Keyboard.KEY_UP) {
-			
-		}else if (key == Keyboard.KEY_DOWN) {
-			
-		}else if (key == Keyboard.KEY_LEFT) {
-			
-		}else if (key == Keyboard.KEY_RIGHT) {
-			
-		}else if(key == Keyboard.KEY_RETURN){
-			
+
+		} else if (key == Keyboard.KEY_DOWN) {
+
+		} else if (key == Keyboard.KEY_LEFT) {
+
+		} else if (key == Keyboard.KEY_RIGHT) {
+
+		} else if (key == Keyboard.KEY_RETURN) {
+
 		}
-		
-		
-		
+
 	}
 
 	@Override
 	protected void onDisplayUpdate() {
-		//刷新帧
-		
-		//遍历土墙集合 将一组
-				Iterator it = mElementList.iterator();
-				while (it.hasNext()) {
-					Element ele = (Element)it.next();
-					ele.draw();
-				}
-				
-		
+		// 刷新帧
+
+		// 遍历土墙集合 将一组
+		Iterator it = mElementList.iterator();
+		while (it.hasNext()) {
+			Element ele = (Element) it.next();
+			ele.draw();
+		}
+
+		myTank.draw();
+
 	}
-	
-	
+
 	public static void playSound(String res) {
 		try {
 			SoundUtils.play(res);
 		} catch (IOException e) {
-			System.out.println("出了问题:"+e.getMessage());
+			System.out.println("出了问题:" + e.getMessage());
 		}
 	}
-	
 
-	public static void drawImg(String res,int x,int y) {
+	public static void drawImg(String res, int x, int y) {
 		try {
-			DrawUtils.draw(res,x, y);
+			DrawUtils.draw(res, x, y);
 		} catch (IOException e) {
-			System.out.println("出了问题:"+e.getMessage());
+			System.out.println("出了问题:" + e.getMessage());
 		}
 	}
-	
 
 }
